b2551eae43adef6c84811c817d5272b8d7827ad1
Manabu Nakazawa <mshibanami+git@gmail.com>
2026-04-03 17:45:29 +1100

    fix: the raw json string is output

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,14 @@ import pino from 'pino';
 
 const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            ignore: 'pid,hostname,time,level',
+            messageFormat: '{msg}',
+        },
+    },
 });
 
 export default logger;
